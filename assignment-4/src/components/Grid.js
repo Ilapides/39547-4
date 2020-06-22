587bbfc2061e153a4be8be503b9e5cdc67fcd1d8
Isaac Lapides <IsaacLapides@gmail.com>
2020-06-22 16:42:12 -0400

    new save1

--- assignment-4/src/components/Grid.js
+++ assignment-4/src/components/Grid.js
@@ -3,27 +3,28 @@ import Row from './Row.js';
 // style
 
 const Grid = ({ rows, color, setCellColor, setActive, activeColoring, activeDrawing, setInactive }) => {
-    
-    return(
-        <tbody
-            className="main-grid"
-            //if mouse leaves gird, sets activeDrawing to false
-            onMouseLeave={function () { setInactive() }}
-            //if mouseup in grid, sets activeDrawing to false
-            onMouseUp={function () { setInactive() }}
-        >
-            {rows.map((col, index) => (<Row
-                key={index}
-                cols={col}
-                color={color}
-                indexr={index}
-                setCellColor={setCellColor}
-                setActive={setActive}
-                activeColoring={activeColoring}
-                activeDrawing={activeDrawing}
-                setInactive={setInactive} />))}
-
-        </tbody>
+   return(
+      <tbody
+         className="grid-table"
+            // //if mouse leaves gird, sets activeDrawing to false
+            // onMouseLeave={function () { setInactive() }}
+            // //if mouseup in grid, sets activeDrawing to false
+            // onMouseUp={function () { setInactive() }}
+      >
+         {rows.map((col, index) => (
+				<Row
+            	key={index}
+               cols={col}
+               color={color}
+               indexr={index}
+               setCellColor={setCellColor}
+               setActive={setActive}
+               activeColoring={activeColoring}
+               activeDrawing={activeDrawing}
+               setInactive={setInactive} 
+				/>
+			))}
+      </tbody>
     )
 };
 
